25fb259545182335790498b9b2c33d7b3ee01943
Yashith Chandeepa <yashithchandeepa123@gmail.com>
2026-02-04 01:28:20 +0530

    feat: Initialize backend project with Prisma ORM, dependencies, and Docker configuration.

--- backend/prisma.js
+++ backend/prisma.js
@@ -1,4 +1,15 @@
 // backend/prisma.js
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
 module.exports = prisma;
